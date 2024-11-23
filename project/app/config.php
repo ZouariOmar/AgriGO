e2a20211636d95f68c03680f5eec93408c9093de
Med Aziz Ben Salem <Medaziz.BENSALEM@esprit.tn>
2024-11-23 17:58:00 +0100

    bro is gay again

--- project/app/config.php
+++ project/app/config.php
@@ -14,7 +14,6 @@ class Config
             $this->db = new PDO("mysql:host={$this->host};dbname={$this->database}", $this->username, $this->password);
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-            echo "Hello";
 
         } catch (PDOException $e) {
             die('Error: Failed to connect to the DB  ' . $e->getMessage());
