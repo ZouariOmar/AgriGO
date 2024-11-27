32debef0ce8fd63957a1793d9bc7d976aff3791d
YoussefSaaidi2004 <157484633+YoussefSaaidi2004@users.noreply.github.com>
2024-11-27 15:40:08 +0100

    DB update

--- project/database/AgriGO.sql
+++ project/database/AgriGO.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 13, 2024 at 06:28 PM
+-- Generation Time: Nov 27, 2024 at 03:39 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -30,11 +30,44 @@ SET time_zone = "+00:00";
 CREATE TABLE `rapports` (
   `Report_ID` int(11) NOT NULL,
   `category` varchar(255) NOT NULL,
-  `sujet` varchar(255) NOT NULL,
+  `subject` varchar(255) NOT NULL,
   `description` text NOT NULL,
-  `piece` varchar(255) NOT NULL,
-  `status` enum('RECIEVED','IN PROCESS','FINISHED') DEFAULT NULL
+  `sta` enum('RECIEVED','IN PROGRESS','DONE') DEFAULT NULL,
+  `Update_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'Update date/time'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rapports`
+--
+
+INSERT INTO `rapports` (`Report_ID`, `category`, `subject`, `description`, `sta`, `Update_at`) VALUES
+(50, 'technical', 'test', 'test', 'RECIEVED', '2024-11-26 16:41:48'),
+(104, 'technical', 'LOREM', 'LOREM', 'RECIEVED', '2024-11-26 16:41:48'),
+(109, 'other', 'ml', 'iokln', 'RECIEVED', '2024-11-26 16:41:48'),
+(110, 'service', 'ujk', 'jnk', 'RECIEVED', '2024-11-26 16:41:48'),
+(111, 'feedback', 'adx', 'zadx', 'RECIEVED', '2024-11-26 16:41:48'),
+(112, 'feedback', 'fbvd', 'gbdfsvcx', 'RECIEVED', '2024-11-26 16:41:48'),
+(116, 'service', 'jjjj', 'mlkj', 'RECIEVED', '2024-11-26 16:42:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rapportstat`
+--
+
+CREATE TABLE `rapportstat` (
+  `StatID` int(11) NOT NULL,
+  `StatRapportID` int(11) NOT NULL,
+  `Status` enum('RECIEVED','IN PROCESS','DONE') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `rapportstat`
+--
+
+INSERT INTO `rapportstat` (`StatID`, `StatRapportID`, `Status`) VALUES
+(1, 112, 'RECIEVED'),
+(5, 116, 'RECIEVED');
 
 --
 -- Indexes for dumped tables
@@ -47,6 +80,13 @@ ALTER TABLE `rapports`
   ADD PRIMARY KEY (`Report_ID`);
 
 --
+-- Indexes for table `rapportstat`
+--
+ALTER TABLE `rapportstat`
+  ADD PRIMARY KEY (`StatID`),
+  ADD KEY `fk_stat` (`StatRapportID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -54,7 +94,23 @@ ALTER TABLE `rapports`
 -- AUTO_INCREMENT for table `rapports`
 --
 ALTER TABLE `rapports`
-  MODIFY `Report_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Report_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
+
+--
+-- AUTO_INCREMENT for table `rapportstat`
+--
+ALTER TABLE `rapportstat`
+  MODIFY `StatID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `rapportstat`
+--
+ALTER TABLE `rapportstat`
+  ADD CONSTRAINT `fk_stat` FOREIGN KEY (`StatRapportID`) REFERENCES `rapports` (`Report_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
